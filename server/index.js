68b40e85f2e668386062d1be04c3518f3c2bdb1d
Владислав <pahnetstarushkami@bk.ru>
2024-04-08 23:56:11 +0300

    new features

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const cors = require("cors")
 const express = require('express')
+const PythonShell = require('python-shell').PythonShell
 
-const app = express()
+
 const port = 80
+const app = express()
 
 
 app.use(cors());
@@ -11,9 +13,22 @@ app.use(express.json());
 
 
 app.post('/python', (req, res) => {
-    fs.writeFileSync('test.py', req.body.code)
-    res.json({message: "success" })
-})
+    fs.writeFileSync('test.py', req.body.code);
+
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+        args: [1, 2, 3]
+      };
+      
+      PythonShell.run('test.py', options, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results);
+      });
+
+    res.json({message: "success" });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
