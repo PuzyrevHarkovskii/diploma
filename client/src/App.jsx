c1f4a94b5eca5f30adae7e1ada17627864dfedd7
Владислав <pahnetstarushkami@bk.ru>
2024-04-08 16:45:10 +0300

    Исправлена ошибка с getValue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,35 @@
 import './App.css';
-import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { python } from '@codemirror/lang-python';
 import "@uiw/codemirror-theme-dracula"
 import {dracula} from "@uiw/codemirror-theme-dracula"
 import {sublime} from "@uiw/codemirror-theme-sublime"
+import { useState } from 'react';
 
-const code = "const a = 0;"
+
+
 
 function App() {
+
+  const [code, setCode] = useState('a = 0');
+
   return (
     <div className='App'>
       <header className='App-header'>
-      <div className='absolute top-20 bottom-40 left-10 right-10'>
+      <div className='absolute top-20 bottom-40 left-10 right-10 text-left'>
         <CodeMirror
         value={code}
         theme={dracula}
         keymap={sublime}
+        mode={python}
         options={{
           theme: "dracula",
           keymap: "sublime",
-          mode: "jsx",
+          mode: "python",
+        }}
+        onChange={(editor, change) => {
+          setCode(editor); //Ошибка с getValue()
+          console.log(code);
         }}
         />
         </div>
