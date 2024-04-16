97b25e8b6b9d0a23fda4e9c93d994f4a30659d90
Владислав <pahnetstarushkami@bk.ru>
2024-04-16 18:53:16 +0300

    Пока работает только передача кода в .py файл с фронтенда, затык.

--- server/index.js
+++ server/index.js
@@ -3,33 +3,32 @@ const cors = require("cors")
 const express = require('express')
 const PythonShell = require('python-shell').PythonShell
 
-
-const port = 80
 const app = express()
+const port = 80
 
 
 app.use(cors());
 app.use(express.json());
 
-
+//Обработка POST-запроса на python:
 app.post('/python', (req, res) => {
     fs.writeFileSync('test.py', req.body.code);
 
     let options = {
-      mode: 'text',
-      pythonOptions: ['-u'], // get print results in real-time
-      args: [1, 2, 3]
-    };
-    
-    PythonShell.run('test.py', options, function (err, results) {
-      if (err) throw err;
-      // results is an array consisting of messages collected during execution
-      console.log('results: %j', results);
-    });
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+        args: [1, 2, 3]
+      };
+      
+      PythonShell.run('test.py', options, function (err, results) {
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results);
+      });
 
-    res.json({message: "success" });
+    res.json({message: "success" }); //успешное выполнение запроса
 });
 
+//Прослушивание порта
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
